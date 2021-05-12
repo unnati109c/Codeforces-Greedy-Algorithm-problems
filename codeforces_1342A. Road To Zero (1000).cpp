a6217525221e67d79a553e89cfd42b076b2b9695
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-05-12 07:15:55 +0530

    1000

---
+++ codeforces_1342A. Road To Zero (1000).cpp
@@ -0,0 +1,43 @@
+// https://codeforces.com/problemset/problem/1342/A
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+ll RoadToZero(ll x, ll y, ll a, ll b)
+{
+	ll ans=0;
+	if(x>y)
+		swap(x,y);
+
+	ans+= a*(y-x);
+
+	if(2*a < b)
+	{
+		ans += x*(2*a);
+	}
+	else
+	{
+		ans += x*b;
+	}
+	return ans;
+
+}
+
+int main()
+{
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll x,y;
+		cin>>x>>y;
+
+		ll a,b;
+		cin>>a>>b;
+
+		cout<<RoadToZero(x,y,a,b)<<endl;
+	}
+
+	return 0;
+}
